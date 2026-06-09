2d92232505d4016136b7658e864a172591a3dd81
Yash Lalchandani <yash.lalchandani@samespace.com>
2026-06-09 15:51:37 +0100

    release: v0.1.0-alpha.3

--- Package.swift
+++ Package.swift
@@ -13,8 +13,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "COrigonSDK",
-            url: "https://github.com/Origon/apple-sdk/releases/download/v0.1.0-alpha.2/COrigonSDK.xcframework.zip",
-            checksum: "5fe8e3a7d7a37555928ccf1d18cfc90ce837e14fa74957599e6f9209ff1a8171"
+            url: "https://github.com/Origon/apple-sdk/releases/download/v0.1.0-alpha.3/COrigonSDK.xcframework.zip",
+            checksum: "519982e61d3036c7e990e24025e33e36f5b9af7964ea478e79a8487c6936d7a0"
         ),
         .target(
             name: "OrigonSDK",
